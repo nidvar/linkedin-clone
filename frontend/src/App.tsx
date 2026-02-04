8b2a3e1dbf5206662ad878e872583f0b9b8f662f
nidvar <djayshine87@hotmail.com>
2026-02-04 13:10:21 +1100

    home, login, signup pages added. routes added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
 import Layout from './components/layout/Layout'
+import { Routes, Route } from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
-    <Layout children={'hello'}></Layout>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/signup' element={<SignUpPage />}/>
+        <Route path='/login' element={<LoginPage />}/>
+      </Routes>
+    </Layout>
   )
 }
 
