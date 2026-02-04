b7b5e1d7e93fbfb2c3ad9d1bf4737c8c9b188c02
nidvar <djayshine87@hotmail.com>
2026-02-04 12:18:14 +1100

    installed tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,14 +6,9 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1 className="text-3xl font-bold underline bg-blue-500">
+        Hello world!
+      </h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
