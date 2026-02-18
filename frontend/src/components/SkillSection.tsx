c764366351f1ecdcf227dcb4981495d0ffb41b2a
nidvar <djayshine87@hotmail.com>
2026-02-18 16:27:19 +1100

    skills section complete

--- frontend/src/components/SkillSection.tsx
+++ frontend/src/components/SkillSection.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import { useState, useEffect, type SubmitEvent } from 'react';
 import type { AuthUserType } from '../utils/types';
 import { useParams } from 'react-router-dom';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postRequest } from '../utils/utilFunctions';
+import { Plus, Trash2 } from 'lucide-react';
 
 function SkillSection({data, ownProfile}: {data: AuthUserType, ownProfile: boolean}) {
   const { username } = useParams();
@@ -10,37 +11,95 @@ function SkillSection({data, ownProfile}: {data: AuthUserType, ownProfile: boole
 
   const [edit, setEdit] = useState(false);
 
-  const [about, setAbout] = useState('');
+  const [skills, setSkills] = useState<string[]>([]);
 
-  const updateSectionMutation = useMutation({
-    mutationFn: async () => {
-      const result = await postRequest('/user/update', {});
-      if (!result.success) throw new Error(result.message);
-      return result;
+  const [skillInput, setSkillInput] = useState('');
+
+  const updateSkillsMutation = useMutation({
+    mutationFn: async (skills: string[]) => {
+      const result = await postRequest('/user/updatedetails', {skills: skills});
+      console.log(result)
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['profile', username] });
+      setEdit(false);
     },
   });
+
+  const addSkills = (skill: string)=>{
+    setSkillInput('');
+    let array = [...skills];
+    array.push(skill);
+    setSkills(array);
+  }
+
+  const deleteSkill = (index: number)=>{
+    let array = [...skills];
+    array.splice(index, 1);
+    setSkills(array);
+  }
+
+  const handleSubmit = async function(e: SubmitEvent){
+    e.preventDefault();
+    updateSkillsMutation.mutate(skills);
+  }
+
+  useEffect(()=>{
+    if(data.skills != undefined || data.skills != null || data.skills != ''){
+      setSkills(data.skills);
+    }
+  }, [data])
 
   return (
     <div className='profile-section shaded-border'>
       <h1 className='font-semibold'>Skills</h1>
-      <div className='profile-section-content'>
+      <div>
         {
-          data.skills.map((item)=>{
+          skills.map((item, index)=>{
             return (
-              <div className='flex gap-2' key={Math.random()}>
-                <p>{item}</p>
-              </div>
-            )
+              <p 
+                key={index} 
+                className='flex items-center gap-5'
+              >
+                {item}{edit?<Trash2 size={14} className='hand-hover' onClick={function(){deleteSkill(index)}}/>:''}
+              </p>
+            );
           })
         }
       </div>
       {
-        ownProfile === true?
-        <button className='edit-button' onClick={function(){setEdit(prev => !prev);}}>{edit?'Cancel':'Edit'}</button>:''
+        edit?
+        <form onSubmit={handleSubmit}>
+          <div className='input-skills' >
+            <input 
+              value={skillInput}
+              placeholder='Add skills' 
+              onChange={function(e){setSkillInput(e.target.value)}}
+            />
+            <Plus className='hand-hover' onClick={function(){addSkills(skillInput)}}/>
+          </div>
+          <button className='mr-3' type="submit">Update</button>
+          <button 
+          className='edit-button' 
+          onClick={
+            function(){
+              setEdit(prev => !prev); 
+              queryClient.invalidateQueries({ queryKey: ['profile', username] });
+            }
+          }
+          >{edit?'Cancel':'Edit'}</button>
+        </form>:''
       }
+      {
+        ownProfile === true?
+        <>
+          {
+            edit === false?
+            <button className='edit-button' onClick={function(){setEdit(prev => !prev)}}>Edit</button>:''
+          }
+        </>:''
+      }
+
     </div>
   )
 }
