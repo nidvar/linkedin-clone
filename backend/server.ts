a45b335ab26a2bd0cf73c2714dbe9e5a0964bcb7
nidvar <djayshine87@hotmail.com>
2026-02-23 13:16:32 +1100

    cors error fix

--- backend/server.ts
+++ backend/server.ts
@@ -46,11 +46,12 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? true
+        ? "https://jarro-linkedin.onrender.com" // your frontend URL
         : "http://localhost:5173",
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
